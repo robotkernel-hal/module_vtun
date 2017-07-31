5ca189a716c1487c9bf8c519554d5da027f651b7
Robert Burger <robert.burger@dlr.de>
2017-07-31 09:36:40 +0200

    switched to transition base state machine

--- src/main/cpp/vtun.cpp
+++ src/main/cpp/vtun.cpp
@@ -1,3 +1,26 @@
+//! robotkernel module for vtun
+/*!
+ * author: Florian Schmidt <florian.schmidt@dlr.de>
+ */
+
+/*
+ * The module_vtun is part of robotkernel.
+ *
+ * robotkernel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * robotkernel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with robotkernel.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #include "vtun.h"
 
 #include <stdio.h>
@@ -8,6 +31,7 @@
 
 #include <sys/syscall.h>
 #include <sys/ioctl.h>
+#include <sys/socket.h>
 #include <linux/if.h>
 #include <linux/if_tun.h>
 
@@ -18,116 +42,143 @@
 
 using namespace std;
 using namespace robotkernel;
+using namespace string_util;
+using namespace module_vtun;
 
-MODULE_DEF(module_vtun, module_vtun::vtun)
-
-namespace module_vtun {
+MODULE_DEF(module_vtun, module_vtun::vtun);
 
 #if !defined(HAVE_GETTID)
 int gettid() {
 #ifdef __NR_gettid
-	return syscall( __NR_gettid );
+    return syscall( __NR_gettid );
 #else
-	return 0;
+    return 0;
 #endif
 }
 #endif
 
 
-vtun::vtun(string name, const YAML::Node& node)
-	: runnable(node), module_base("vtun", name, node) {
-
-	fd = -1;
-	write_to = get_as<string>(node, "write_to");
-	mtu = get_as<unsigned int>(node, "mtu", 1024*10);
+vtun::vtun(string name, const YAML::Node& node) : 
+    runnable(node), module_base("vtun", name, node) 
+{
+    fd = -1;
+    write_to = get_as<string>(node, "write_to");
+    mtu = get_as<unsigned int>(node, "mtu", 1024*10);
 }
 
 vtun::~vtun() {
-	if(state != module_state_init)
-		set_state(module_state_init);
+    if(state != module_state_init)
+        set_state(module_state_init);
 }
 
-
+// set state
 int vtun::set_state(module_state_t requested_state) {
-	log(info, "state %s requested\n", state_to_string(requested_state));
-	switch (requested_state) {
-	case module_state_init: {
-		stop();
-		if(fd != -1) {
-			close(fd);
-			fd = -1;
-		}
-		break;
-        }
-        case module_state_preop: {
+    kernel& k = *kernel::get_instance();
 
-		kernel* k = kernel::get_instance();
-		write_to_mod = k->get_module(write_to.c_str());
-		if(!write_to_mod)
-			throw ::str_exception_tb("could not get write_to module named %s", repr(write_to).c_str());
-		
-		fd = open("/dev/net/tun", O_RDWR);
-		if(fd == -1)
-			throw errno_exception_tb("could not open /dev/net/tun");
-		
-		struct ifreq ifr;
-		memset(&ifr, 0, sizeof(ifr));
-		/* Flags: IFF_TUN   - TUN device (no Ethernet headers) 
-		 *        IFF_TAP   - TAP device  
-		 *
-		 *        IFF_NO_PI - Do not provide packet information  
-		 */ 
-		ifr.ifr_flags = IFF_TUN; 
+    // get transition
+    uint32_t transition = GEN_STATE(this->state, state);
 
-		if(ioctl(fd, TUNSETIFF, (void *)&ifr))
-			throw errno_exception_tb("could not request tun/tap device\n");
-		if_name = ifr.ifr_name;
-		log(info, "using interface %s\n", if_name.c_str());
-		break;
+    switch (transition) {
+        case op_2_safeop:
+        case op_2_preop:
+        case op_2_init:
+            // ====> stop sending commands
+            if (    (transition == op_2_safeop))
+                break;
+        case safeop_2_preop:
+        case safeop_2_init:
+            // ====> stop receiving measurements
+            stop();
+
+            if (    (transition == op_2_preop) ||
+                    (transition == safeop_2_preop))
+                break;
+        case preop_2_init:
+            // ====> deinit devices
+            close(fd);
+            fd = -1;
+        case init_2_init:
+            // ====> do nothing
+            break;
+
+        case init_2_op:
+        case init_2_safeop:
+        case init_2_preop: {
+            // ====> initial devices            
+            write_to_stream = k.get_stream(write_to);
+            if(!write_to_stream)
+                throw ::str_exception_tb("could not get write_to module named %s", repr(write_to).c_str());
+
+            fd = open("/dev/net/tun", O_RDWR);
+            if(fd == -1)
+                throw errno_exception_tb("could not open /dev/net/tun");
+
+            struct ifreq ifr;
+            memset(&ifr, 0, sizeof(ifr));
+            /* Flags: IFF_TUN   - TUN device (no Ethernet headers) 
+             *        IFF_TAP   - TAP device  
+             *
+             *        IFF_NO_PI - Do not provide packet information  
+             */ 
+            ifr.ifr_flags = IFF_TUN; 
+
+            if(ioctl(fd, TUNSETIFF, (void *)&ifr))
+                throw errno_exception_tb("could not request tun/tap device\n");
+            if_name = ifr.ifr_name;
+            log(info, "using interface %s\n", if_name.c_str());
+
+            if (    (transition == init_2_preop))
+                break;
         }
-        case module_state_safeop: {
-		start();
-		break;
-        }
-        case module_state_op:
-		break;
-	default:        
-		// invalid state 
-		// nothing todo
-		// return -1;
-		break;
-	}
-	state = requested_state;
-	
-	return 0;
+        case preop_2_op:
+        case preop_2_safeop:
+            // ====> start receiving measurements
+            start();
+
+            if (    (transition == init_2_safeop) ||
+                    (transition == preop_2_safeop))
+                break;
+        case safeop_2_op:
+            // ====> start sending commands
+            break;
+        case op_2_op:
+        case safeop_2_safeop:
+        case preop_2_preop:
+            // ====> do nothing
+            break;
+
+        default:
+            break;
+    }
+
+    return (this->state = state);
 }
 
 void vtun::run() {
-	log(info, "vtun tid %d started!\n", gettid());
-	
-	vector<char> receive_buffer(mtu);
-	while (running()) {
-		int len = ::read(fd, &receive_buffer[0], mtu);
-		if(len == -1)
-			throw errno_exception_tb("failed to read max %d bytes from tun device", mtu);
-		log(verbose, "got net packet of length %d\n", len);
-		
-		int n = write_to_mod->write(&receive_buffer[0], (size_t)len);
-		if(n != len)
-			log(warning, "wanted to write %d bytes to %s->write() returned %d\n", len, write_to.c_str(), n);
-	}
+    log(info, "vtun tid %d started!\n", gettid());
 
-	log(info, "vtun handler %d stopping\n", gettid());
+    vector<char> receive_buffer(mtu);
+    while (running()) {
+        int len = ::read(fd, &receive_buffer[0], mtu);
+        if(len == -1)
+            throw errno_exception_tb("failed to read max %d bytes from tun device", mtu);
+        log(verbose, "got net packet of length %d\n", len);
+
+        int n = write_to_stream->write(&receive_buffer[0], (size_t)len);
+        if(n != len)
+            log(warning, "wanted to write %d bytes to %s->write() returned %d\n", len, write_to.c_str(), n);
+    }
+
+    log(info, "vtun handler %d stopping\n", gettid());
 }
 
 size_t vtun::write(void* buf, size_t bufsize) {
-	// send packet!
-	int n = ::write(fd, buf, bufsize);
-	if(n == -1)
-		throw errno_exception_tb("write(%d bytes)", bufsize);
-	if(n != (int)bufsize)
-		log(warning, "wanted to write %d bytes, write() returned %d\n", bufsize, n);
-	return n;
+    // send packet!
+    int n = ::write(fd, buf, bufsize);
+    if(n == -1)
+        throw errno_exception_tb("write(%d bytes)", bufsize);
+    if(n != (int)bufsize)
+        log(warning, "wanted to write %d bytes, write() returned %d\n", bufsize, n);
+    return n;
 }
 
-}
--- src/main/cpp/vtun.h
+++ src/main/cpp/vtun.h
@@ -1,3 +1,25 @@
+//! robotkernel module for vtun
+/*!
+ * author: Florian Schmidt <florian.schmidt@dlr.de>
+ */
+
+/*
+ * The module_vtun is part of robotkernel.
+ *
+ * robotkernel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * robotkernel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with robotkernel.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef VTUN_H
 
 #include <string>
@@ -10,28 +32,47 @@
 #include <yaml-cpp/yaml.h>
 
 namespace module_vtun {
+#ifdef EMACS
+}
+#endif
 
 using namespace std;
 
-class vtun
-	: public robotkernel::runnable, 
-	  public robotkernel::module_base {
+class vtun : 
+    public robotkernel::runnable, 
+    public robotkernel::module_base 
+{
+    private:
+        int fd;                                     //!< tun device file descriptor
+        string if_name;                             //!< tun interface name
+        string write_to;                            //!< robotkernel stream device to write to
+        robotkernel::sp_stream_t write_to_stream;   //!< robotkernel stream deivce
+        unsigned int mtu;                           //!< max mtu size
 
-	int fd;
-	string if_name;
-	string write_to;
-	robotkernel::module* write_to_mod;
-	unsigned int mtu;
-        void run();
-public:
+        void run();                                 //!< packet receive thread
+
+    public:
+        //! de-/construction
+        /*
+         * \param name vtun name
+         * \param node YAML configuration node
+         */
         vtun(string name, const YAML::Node& node);
         ~vtun();
 
-	int set_state(module_state_t requested_state);
-	size_t write(void* buf, size_t bufsize);
+        //! set state
+        /*!
+         * \param state new state
+         */
+        int set_state(module_state_t requested_state);
 
+        size_t write(void* buf, size_t bufsize);
 };
 
+#ifdef EMACS
+{
+#endif
 }
 
 #endif // VTUN_H
+
